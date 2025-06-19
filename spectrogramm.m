479cae28ab4b9c6a9817370b97229a2054c4e6b6
bossersimon <74986361+bossersimon@users.noreply.github.com>
2025-06-19 13:53:15 +0200

    added model to compare angle estimate with
    The average frequency over the measurement is used in the model. Measurements might need some cleaning

--- spectrogramm.m
+++ spectrogramm.m
@@ -2,7 +2,7 @@
 
 %% Mex spectogram stuff
 
-M = readmatrix("bil1.txt");
+M = readmatrix("bil2.txt");
 
 Mx = M(:,1);
 My = M(:,2);
@@ -18,21 +18,46 @@ w = rectwin(wsize);
 
 [s,f,t] = spectrogram(Mx,w,ovlap,Ndft,fs);
 
+figure;
 waterplot(s,f,t)
 
 th = 0.5;   % threshold frequency in herz
 th_idx = round(th/(fs/Ndft)) +1;   % index of roughly this frequency
 threshold = (th_idx-1)*fs/Ndft;    % actual threshold with this index
 
-%phi = zeros(1,size(s,2)); 
 
 [~,f0_relative_idx] = max(abs(s(th_idx:end,:)));  % returns relative index of masked array
 f0_idx = f0_relative_idx + th_idx - 1;            % corresponding index in full array
 
+f_avg = mean(f(f0_idx(15:end)))
+%f_avg = mean(f(f0_idx(15:end)))
+wheel_radius = 2;
+v_avg = f_avg*wheel_radius*3.6
+
 cols = 1:size(s,2);   % cols for sub2ind
 S = s(sub2ind(size(s), f0_idx, cols));  % values of S at wanted frequencies
 
+tol = 1e-6;
+S(abs(S) < tol) = 0;
+
 phi_x = angle(S);
+phix_unwrapped = -unwrap(phi_x);
+
+
+model_x = cos(f_avg*2*pi*t);
+x_analytic = hilbert(model_x);
+phi_est = unwrap(angle(x_analytic));
+%phi_est = acos(model_x);
+
+figure;
+plot(phix_unwrapped)
+hold on
+plot(model_x)
+plot(phi_est)
+legend('phi','x(t)','phi_{model}')
+hold off
+
+
 
 function waterplot(s,f,t)
 % Waterfall plot of spectrogram
