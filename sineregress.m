4eff089f528e2ad4636334f42ac45ae9434a1cbd
bossersimon <simon.bosser@gmail.com>
2025-09-01 17:27:39 +0200

    curve fit works in this example, maybe try on recorded data

---
+++ sineregress.m
@@ -0,0 +1,16 @@
+
+%% Test
+clear all
+
+t = (1:1000)';
+X = ones(1000,3);
+X(:,2) = cos((2*pi)/50*t);
+X(:,3) = sin((2*pi)/50*t);
+y = 2*cos((2*pi)/50*t-pi/4+randn(size(t)));
+y = y(:);
+beta = X\y;
+yhat = beta(1)+beta(2)*cos((2*pi)/50*t) + beta(3)*sin((2*pi)/50*t);
+
+plot(t,y,'b');
+hold on
+plot(t,yhat,'r','LineWidth',2);
