de6e6c5aa3f9d8c99b3f6e1b51fb6bd4c75c86d1
bossersimon <simon.bosser@gmail.com>
2025-09-17 17:54:39 +0200

    fit works with chirp and changing offset

--- linearfit.m
+++ linearfit.m
@@ -3,21 +3,25 @@
 clear all 
 
 t = 0:1/100:40';
-f = 2;
+f1 = 2;
 phi_offs = 0;
-x = cos(2*pi*f*t) + 0.1*rand(size(t)) + 0.02*t;
-y = sin(2*pi*f*t) + 0.1*rand(size(t)) - 0.02*t;
+%x = cos(2*pi*f1*t) + 0.1*rand(size(t)) + 0.02*t;
+%y = sin(2*pi*f1*t) + 0.1*rand(size(t)) - 0.02*t;
+
+%x = chirp(t,0,40,5)+0.05*rand(size(t)) + 0.02*t;
+%y = chirp(t,0,40,5)+0.05*rand(size(t)) - 0.02*t;
 
 plot(t,x);
 hold on
 plot(t,y);
+legend('x','y')
 
 %% 
 
 % Linear model: Ccos(wt) + Dsin(wt) + B
 
 window_size=30;
-step = 10;
+step = 30;
 N = length(y); % recording length
 
 n = floor((N - window_size) / step) + 1; % number of fits
@@ -31,21 +35,51 @@ yhat = zeros(n,window_size);
 t_vec = zeros(n,window_size);
 
 A = ones(window_size,3);     % design matrix
-
 t_win = t(1:window_size);
-A(:,1) = cos(2*pi*f*t_win);
-A(:,2) = sin(2*pi*f*t_win);
 y = y(:);
 
-for k = 1:n
-    start_idx = (k-1)*step +1;
-    j = start_idx:start_idx+ window_size -1; % slice
-    betas(k,:) = A\y(j);
-    
-    t_vec(k,:) = t(j);
-    yhat(k,:) = betas(k,1)*cos(2*pi*f*t_win) + betas(k,2)*sin(2*pi*f*t_win) + betas(k,3);
-end
+% Now need to iterate over different f
 
+f0 = 0; % starting frequency
+delta = 1; % 2*delta search range
+best_y = zeros(n,window_size);
+
+y_fit = zeros(1,window_size);
+y_best = zeros(1,window_size);
+    
+
+for k = 1:n
+    best_err = inf;
+    best_f = 0;
+    
+    f_candidates = linspace(f0 - delta, f0 + delta, 100);
+
+    for f = f_candidates
+        A(:,1) = cos(2*pi*f*t_win);
+        A(:,2) = sin(2*pi*f*t_win);
+
+        start_idx = (k-1)*step +1;
+        j = start_idx:start_idx+ window_size -1; % slice
+        betas(k,:) = A\y(j);
+        
+        y_fit = betas(k,1)*cos(2*pi*f*t_win) + betas(k,2)*sin(2*pi*f*t_win) + betas(k,3);
+
+        err = immse(y(j)',y_fit);
+
+        if err<best_err
+            best_err = err;
+            best_f = f;
+            y_best = y_fit;
+        end
+    end
+
+    f0 = best_f; % update search range
+
+    yhat(k,:) = y_best; % store results
+    t_vec(k,:) = t(j);
+
+end 
+    
 
 %% plotting
 
